bd1db81cd5dcf622a88cf3362397c934177a0291
i6Grja3R <cyktm19814@yahoo.co.jp>
2024-11-01 02:54:54 +0900

    update13

--- bbs_que_answer.php
+++ bbs_que_answer.php
@@ -405,12 +405,17 @@ $noimage_url = $upload_dir['baseurl'] . '/noimage.png';
         //divQuestionHeaderPartial.classList.add("questionHeader-partial");
         //div.appendChild(child); // div の末尾に child を追加
 
+        /* アイコン画像要素作成 */
         const comment_area = document.createElement("div"); // const image_area の下から移動
         const divUserArea = document.createElement("div"); // div (子)を生成
+        const divUserIcon = document.createElement("div"); // div (子)を生成
         divUserArea.classList.add("user-area"); // classの追加
-        divUserArea.appendChild(child); // div (子要素) の末尾に child を追加
+        divUserIcon.classList.add("user-icon"); // classの追加
+        divUserIcon.appendChild(child); // div (子要素) の末尾に child を追加
+        divUserArea.appendChild(divUserIcon); // div (子要素) の末尾に div を追加
         comment_area.appendChild(divUserArea); // comment_area (親要素) の末尾に div を追加
 
+        /* 名前要素作成 */
         const divAnswerNameArea = document.createElement("div"); // div (子)を生成
         divAnswerNameArea.classList.add("answer-name-area"); // classの追加
         child = document.createElement("p"); // p (孫)を生成
@@ -420,6 +425,7 @@ $noimage_url = $upload_dir['baseurl'] . '/noimage.png';
         divAnswerNameArea.appendChild(child); // div (子要素) の末尾に child を追加
         comment_area.appendChild(divAnswerNameArea); // comment_area (親要素) の末尾に div を追加
 
+        /* コメント要素作成 */
         const divAnswerTextArea = document.createElement("div"); // div (子)を生成
         divAnswerTextArea.classList.add("answer-text-area"); // classの追加
         child = document.createElement("p"); // p (孫)を生成
@@ -428,6 +434,7 @@ $noimage_url = $upload_dir['baseurl'] . '/noimage.png';
         comment_area.appendChild(divAnswerTextArea); // comment_area (親要素) の末尾に div を追加
         divAnswerTextArea.style.fontSize = "150%"; //コメントの文字のサイズ
 
+        /* ファイルアップロード要素作成 */
         const image_area = document.createElement("div");
         var image_count = 0;
 
@@ -469,10 +476,12 @@ $noimage_url = $upload_dir['baseurl'] . '/noimage.png';
         }
         image_area.appendChild(divUploadfileArea); // image_area (親要素) の末尾に div を追加
 
+        /* アップロードファイルサイズ制限事項要素作成 */
         const divFilesizeRestrictionArea = document.createElement("div"); // div (子)を生成
         divFilesizeRestrictionArea.classList.add("filesize-restriction-area"); // classの追加
         comment_area.appendChild(divFilesizeRestrictionArea); // comment_area (親要素) の末尾に filesize-restriction-area を追加
 
+        /* 確認画面送信ボタン要素作成 */
         const divPostButton = document.createElement("div"); // div (子)を生成
         divPostButton.classList.add("post-button"); // classの追加
 
