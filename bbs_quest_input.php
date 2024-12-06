585ea1d2e6dff935d26aee575693d64a4c3b37f1
i6Grja3R <cyktm19814@yahoo.co.jp>
2024-12-06 23:30:05 +0900

    update68

--- bbs_quest_input.php
+++ bbs_quest_input.php
@@ -319,9 +319,9 @@ $noimage_url = $upload_dir['baseurl'] . '/noimage.png';
                     divImagePartial.style.float = "left";
                 } else if (image_count == 2) {
                     divImagePartial.style.float = "left";
-                    divBodyPartialParts.style.height = "728px"; //コメント欄外枠
+                    divBodyPartialParts.style.height = "728px"; // コメント欄外枠
                 } else if (image_count == 3) {
-                    Array.from(divImagePartial.children).forEach(x => x.style.float = "left");
+                    Array.from(divImagePartial.children).forEach(x => x.style.float = "left"); // divImagePartial の子要素を取得する、与えられた関数を配列の各要素に対して一度ずつ実行
                 }
                 input_area.style.display = "none";
                 confirm_area.style.display = "block";
