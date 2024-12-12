9975e5adcf5177c3be431a5bc4540edb9ca6151f
i6Grja3R <cyktm19814@yahoo.co.jp>
2024-12-12 19:14:53 +0900

    Update 82

--- functions.php
+++ functions.php
@@ -1,4 +1,366 @@
 <?php
+// ↓ ここから追記
+// rel="prev"とrel=“next"表示の削除
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+
+// WordPressバージョン表示の削除
+remove_action('wp_head', 'wp_generator');
+
+// 絵文字表示のための記述削除（絵文字を使用しないとき）
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+// アイキャッチ画像の有効化
+add_theme_support('post-thumbnails');
+
+// 投稿固定ページを旧式のエディターに戻す
+add_filter('gutenberg_can_edit_post_type', '__return_false'); //Gutenbergプラグイン用
+add_filter('use_block_editor_for_post', '__return_false'); //WordPressブロックエディター用
+
+//従来のウィジェットエディターに戻す
+function example_theme_support()
+{
+    remove_theme_support('widgets-block-editor');
+}
+add_action('after_setup_theme', 'example_theme_support');
+
+// ウィジェット追加
+if (function_exists('register_sidebar')) {
+    register_sidebar(array(
+        'name' => 'ウィジェット１',
+        'id' => 'widget01',
+        'before_widget' => '<div class=”widget”>',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    ));
+}
+
+// 自動成型削除
+function my_tiny_mce_before_init($init_array)
+{
+    global $allowedposttags;
+    $init_array['valid_elements'] = '*[*]';
+    $init_array['extended_valid_elements'] = '*[*]';
+    $init_array['valid_children'] = '+a[' . implode('|', array_keys($allowedposttags)) . ']';
+    // $init_array['indent'] = true;
+    if (is_page()) {
+        $init_array['wpautop'] = false;
+        $init_array['force_p_newlines'] = false;
+    }
+    return $init_array;
+}
+add_filter('tiny_mce_before_init', 'my_tiny_mce_before_init');
+
+function custom_print_scripts()
+{
+    if (!is_admin()) {
+        //デフォルトjquery削除
+        wp_deregister_script('jquery');
+
+        //GoogleCDNから読み込む
+        wp_enqueue_script('jquery-js', '//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js');
+        wp_enqueue_script('archive-js', get_template_directory_uri() . '/js/archive.js');
+    }
+}
+add_action('wp_print_scripts', 'custom_print_scripts');
+
+//ヘッダーにCDNを読み込ませる
+function fontawesome_enqueue()
+{
+    wp_enqueue_script('fontawesome_script', 'https://kit.fontawesome.com/82bcc49272.js');
+}
+add_action('wp_enqueue_scripts', 'fontawesome_enqueue');
+
+function register_stylesheet()
+{
+    wp_register_style('reset', get_template_directory_uri() . '/css/destyle.css');
+    wp_register_style('style', get_template_directory_uri() . '/css/style.css');
+    wp_register_style('base', 'https://use.fontawesome.com/releases/v6.2.0/css/all.css');
+}
+function add_stylesheet()
+{
+    register_stylesheet();
+    wp_enqueue_style('reset', '', array(), '1.0', false);
+    wp_enqueue_style('style', '', array(), '1.0', false);
+    wp_enqueue_style('base', '', array(), '1.0', false);
+}
+add_action('wp_enqueue_scripts', 'add_stylesheet');
+
+/*---- Google Icon ----*/
+function add_google_icons()
+{
+    wp_register_style(
+        'googleFonts',
+        'https://fonts.googleapis.com/icon?family=Material+Icons'
+    );
+    wp_enqueue_style('googleFonts');
+}
+add_action('wp_enqueue_scripts', 'add_google_icons');
+
+//PHPをウィジェット追加
+function widget_text_exec_php($widget_text)
+{
+    if (strpos($widget_text, '<' . '?') !== false) {
+        ob_start();
+        eval('?>' . $widget_text);
+        $widget_text = ob_get_contents();
+        ob_end_clean();
+    }
+    return $widget_text;
+}
+add_filter('widget_text', 'widget_text_exec_php', 99);
+
+// カスタマイズコメントフォーム
+if (!function_exists('custom_comment_form')) {
+    function custom_comment_form($args)
+    {
+        // 「コメントを残す」を削除
+        $args['title_reply'] = '';
+        //コメント欄の前に表示する文字列の削除　※デフォルトではコメント
+        $args['comment_field'] = '<p class="comment-form-comment"><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>';
+        //「admin としてログイン中。ログアウトしますか ? * が付いている欄は必須項目です」を削除
+        $args['logged_in_as'] = '';
+        // 「メールアドレスが公開されることはありません」を削除
+        $args['comment_notes_before'] = '';
+        return $args;
+    }
+}
+
+add_filter('comment_form_defaults', 'custom_comment_form');
+// カスタマイズコメントフォームフィールド
+if (!function_exists('custom_comment_form_fields')) {
+    function custom_comment_form_fields($arg)
+    {
+        // コメントからウェブサイトとEmailを削除
+        $arg['url'] = '';
+        $arg['email'] = '';
+        return $arg;
+    }
+}
+add_filter('comment_form_default_fields', 'custom_comment_form_fields');
+
+// ----------------------------------------------------------------------------
+// コメント欄の名前が未入力の場合の投稿者名
+// ----------------------------------------------------------------------------
+function default_author_name($author, $comment_ID, $comment)
+{
+    if ($author == __('Anonymous')) {
+        $author = '名無しさん';
+    }
+
+    return $author;
+}
+add_filter('get_comment_author', 'default_author_name', 10, 3);
+
+// 検索ワードファイルパス
+define('SEARCH_WORDS_FILE_PATH', __DIR__ . '/test.csv');
+
+
+/* 投稿と固定ページ一覧にスラッグの列を追加 */
+function add_posts_columns_slug($columns)
+{
+    $columns['slug'] = 'スラッグ';
+    echo '';
+    return $columns;
+}
+add_filter('manage_posts_columns', 'add_posts_columns_slug');
+add_filter('manage_pages_columns', 'add_posts_columns_slug');
+
+/* スラッグを表示 */
+function custom_posts_columns_slug($column_name, $post_id)
+{
+    if ($column_name == 'slug') {
+        $post = get_post($post_id);
+        $slug = $post->post_name;
+        echo esc_attr($slug);
+    }
+}
+add_action('manage_posts_custom_column', 'custom_posts_columns_slug', 10, 2);
+add_action('manage_pages_custom_column', 'custom_posts_columns_slug', 10, 2);
+
+
+//アイキャッチを有効化
+add_theme_support('post-thumbnails');
+
+//画像サイズ追加
+add_image_size('rect', 640, 400, true);
+
+//画像URLからIDを取得
+function get_attachment_id_by_url($url)
+{
+    global $wpdb;
+    $sql = "SELECT ID FROM {$wpdb->posts} WHERE post_name = %s";
+    preg_match('/([^\/]+?)(-e\d+)?(-\d+x\d+)?(\.\w+)?$/', $url, $matches);
+    $post_name = $matches[1];
+    return (int)$wpdb->get_var($wpdb->prepare($sql, $post_name));
+}
+
+//画像をサムネイルで出力
+function catch_that_image()
+{
+    global $post;
+    $first_img = '';
+    $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+    $first_img_src = $matches[1][0];
+    $attachment_id = get_attachment_id_by_url($first_img_src);
+    $first_img = wp_get_attachment_image($attachment_id, 'rect', false, array('class' => 'archive-thumbnail'));
+    if (empty($first_img)) {
+        $first_img = '<img class="attachment_post_thumbnail" src="' . get_stylesheet_directory_uri() . '/assets/img/common/no-img.jpg" alt="No image" />';
+    }
+    return $first_img;
+}
+
+add_filter('use_block_editor_for_post', '__return_false');
+add_theme_support('post-thumbnails');
+
+// アクセス数をカウントする
+function set_post_views_days()
+{
+    $postID = get_the_ID();
+    $key = 'pv_count_week';
+    set_post_views($postID, $key);
+    $key = 'pv_count_3day';
+    set_post_views($postID, $key);
+}
+function set_post_views($postID, $key)
+{
+    $sum_count = (int) get_post_meta($postID, $key, true);
+    update_post_meta($postID, $key, $sum_count + 1);
+}
+
+//ボットの判別
+function isBot()
+{
+    $bot_list = array(
+        'Googlebot',
+        'Yahoo! Slurp',
+        'Mediapartners-Google',
+        'msnbot',
+        'bingbot',
+        'MJ12bot',
+        'Ezooms',
+        'pirst; MSIE 8.0;',
+        'Google Web Preview',
+        'ia_archiver',
+        'Sogou web spider',
+        'Googlebot-Mobile',
+        'AhrefsBot',
+        'YandexBot',
+        'Purebot',
+        'Baiduspider',
+        'UnwindFetchor',
+        'TweetmemeBot',
+        'MetaURI',
+        'PaperLiBot',
+        'Showyoubot',
+        'JS-Kit',
+        'PostRank',
+        'Crowsnest',
+        'PycURL',
+        'bitlybot',
+        'Hatena',
+        'facebookexternalhit',
+        'NINJA bot',
+        'YahooCacheSystem',
+        'NHN Corp.',
+        'Steeler',
+        'DoCoMo',
+    );
+    $is_bot = false;
+    foreach ($bot_list as $bot) {
+        if (stripos($_SERVER['HTTP_USER_AGENT'], $bot) !== false) {
+            $is_bot = true;
+            break;
+        }
+    }
+    return $is_bot;
+}
+
+function getPostViewsBase($postID, $count_key)
+{
+    $count = get_post_meta($postID, $count_key, true);
+    if ('' == $count) {
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+
+        return '0 View';
+    }
+
+    return $count . ' Views';
+}
+function getPostViewsWeek($postID)
+{
+    return getPostViewsBase($postID, 'pv_count_week');
+}
+function getPostViews3day($postID)
+{
+    return getPostViewsBase($postID, 'pv_count_3 day');
+}
+
+// 7日毎にアクセスをリセットして人気カテゴリーを表示
+//クリック数をカウントする
+function category_views_week()
+{
+    global $cat;
+    $categoryID = $cat;
+    $key = 'category_count_week';
+    category_views($categoryID, $key);
+}
+
+function category_views($categoryID, $key)
+{
+    $sum_count = (int) get_term_meta($categoryID, $key, true);
+    update_term_meta($categoryID, $key, $sum_count + 1);
+}
+
+function display_maintenance()
+{
+    echo <<<maintenance
+<hr>
+ただいまメンテナンス中です。<br>
+しばらく時間をおいてアクセスしてください。
+<hr>
+maintenance;
+
+    ini_set("display_errors", "On");
+}
+
+add_action("phpmailer_init", "send_smtp_email");
+function send_smtp_email($phpmailer)
+{
+    $phpmailer->isSMTP();
+    $phpmailer->Host       = "mail.last.cfbx.jp";
+    $phpmailer->SMTPAuth   = true;
+    $phpmailer->Port       = 587;
+    $phpmailer->SMTPSecure = "tls";
+    $phpmailer->Username   = "test@last.cfbx.jp";
+    $phpmailer->Password   = "takuya7530";
+    $phpmailer->From       = "test@last.cfbx.jp";
+    $phpmailer->FromName   = "test";
+}
+
+function setToken()
+{
+    global $_SESSION;
+    if (!isset($_SESSION['csrf_token'])) {
+        // bin2hexはphp7.0,openssl_random_pseudo_bytesはphp5.3
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(16));
+    }
+    // スクリプト自体の実行を終了
+    return;
+}
+function getToken()
+{
+    global $_SESSION;
+    $token = null;
+    if (isset($_SESSION['csrf_token'])) {
+        $token = $_SESSION['csrf_token'];
+    }
+
+    return $token;
+}
+
 function enquiry_sample()
 {
     header('Content-type: application/json; charset=UTF-8');
@@ -280,7 +642,7 @@ function bbs_que_list_items()
                     break;
             }
         }
-        $result['items'][] = ['title' => $row->title, 'img1' => $url, 'type' => $type, 'url' => home_url('質問みる?' . $row->unique_id)];
+        $result['items'][] = ['title' => $row->title, 'img1' => $url, 'type' => $type, 'url' => home_url('質問回答画面?' . $row->unique_id)];
     }
     echo json_encode($result);
     exit;
